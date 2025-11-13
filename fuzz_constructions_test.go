9205df18f3f73131d7f647f6fa0656fe4011c59e
Coda Hale <coda.hale@gmail.com>
2025-11-12 19:52:48 -0700

    test: check posterior states of constructions

--- fuzz_constructions_test.go
+++ fuzz_constructions_test.go
@@ -8,42 +8,50 @@ import (
 )
 
 func FuzzStream(f *testing.F) {
-	encrypt := func(key []byte, nonce []byte, message []byte) []byte {
+	encrypt := func(key []byte, nonce []byte, message []byte) (ciphertext, state []byte) {
 		protocol := lockstitch.NewProtocol("stream")
 		protocol.Mix("key", key)
 		protocol.Mix("nonce", nonce)
-		return protocol.Encrypt("message", message[:0], message)
+		return protocol.Encrypt("message", message[:0], message), protocol.Derive("state", nil, 8)
 	}
 
-	decrypt := func(key []byte, nonce []byte, message []byte) []byte {
+	decrypt := func(key []byte, nonce []byte, message []byte) (plaintext, state []byte) {
 		protocol := lockstitch.NewProtocol("stream")
 		protocol.Mix("key", key)
 		protocol.Mix("nonce", nonce)
-		return protocol.Decrypt("message", message[:0], message)
+		return protocol.Decrypt("message", message[:0], message), protocol.Derive("state", nil, 8)
 	}
 
 	f.Add([]byte("yellow submarine"), []byte("12345678"), []byte("hello world"))
 	f.Fuzz(func(t *testing.T, key []byte, nonce []byte, message []byte) {
-		ciphertext := encrypt(key, nonce, message)
-		if got, want := decrypt(key, nonce, ciphertext), message; !bytes.Equal(got, want) {
+		ciphertext, stateA := encrypt(key, nonce, message)
+		plaintext, stateB := decrypt(key, nonce, ciphertext)
+		if got, want := plaintext, message; !bytes.Equal(got, want) {
 			t.Errorf("decrypt(key, nonce, ciphertext) = %v, want = %v", got, want)
+		}
+		if !bytes.Equal(stateA, stateB) {
+			t.Errorf("divergent posterior protocol states: %v != %v", stateA, stateB)
 		}
 	})
 }
 
 func FuzzAEAD(f *testing.F) {
-	encrypt := func(key []byte, nonce []byte, message []byte) []byte {
+	encrypt := func(key []byte, nonce []byte, message []byte) (ciphertext, state []byte) {
 		protocol := lockstitch.NewProtocol("aead")
 		protocol.Mix("key", key)
 		protocol.Mix("nonce", nonce)
-		return protocol.Seal("message", nil, message)
+		return protocol.Seal("message", nil, message), protocol.Derive("state", nil, 8)
 	}
 
-	decrypt := func(key []byte, nonce []byte, message []byte) ([]byte, error) {
+	decrypt := func(key []byte, nonce []byte, message []byte) (plaintext, state []byte, err error) {
 		protocol := lockstitch.NewProtocol("aead")
 		protocol.Mix("key", key)
 		protocol.Mix("nonce", nonce)
-		return protocol.Open("message", nil, message)
+		plaintext, err = protocol.Open("message", nil, message)
+		if err != nil {
+			return nil, nil, err
+		}
+		return plaintext, protocol.Derive("state", nil, 8), nil
 	}
 
 	f.Add([]byte("yellow submarine"), []byte("12345678"), []byte("hello world"), uint(2), byte(100))
@@ -52,10 +60,10 @@ func FuzzAEAD(f *testing.F) {
 			t.Skip()
 		}
 
-		c := encrypt(key, nonce, plaintext)
+		c, stateA := encrypt(key, nonce, plaintext)
 
 		// check for decryption of authentic ciphertext
-		p2, err := decrypt(key, nonce, c)
+		p2, stateB, err := decrypt(key, nonce, c)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -63,10 +71,14 @@ func FuzzAEAD(f *testing.F) {
 			t.Errorf("decrypt(key, nonce, c) = %v, want = %v", got, want)
 		}
 
+		if !bytes.Equal(stateA, stateB) {
+			t.Errorf("divergent posterior protocol states: %v != %v", stateA, stateB)
+		}
+
 		// check for non-decryption of inauthentic ciphertext
 		c[int(idx)%len(c)] ^= mask
 
-		if got, err := decrypt(key, nonce, c); err == nil {
+		if got, _, err := decrypt(key, nonce, c); err == nil {
 			t.Errorf("decrypt(key, nonce, c) = %v, want = nil", got)
 		}
 	})
