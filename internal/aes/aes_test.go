6ab0bd30dd25161bcbaefc48ac0d6523e0ffbe78
Coda Hale <coda.hale@gmail.com>
2026-01-05 16:07:30 -0700

    Add some comprehensive benchmarks for the aes package.

--- internal/aes/aes_test.go
+++ internal/aes/aes_test.go
@@ -40,3 +40,34 @@ func FuzzCTR(f *testing.F) {
 		}
 	})
 }
+
+func BenchmarkCTR(b *testing.B) {
+	key := make([]byte, 16)
+	iv := make([]byte, aes.BlockSize)
+
+	for _, length := range lengths {
+		b.Run(length.name, func(b *testing.B) {
+			msg := make([]byte, length.n)
+			b.SetBytes(int64(length.n))
+			b.ReportAllocs()
+			for b.Loop() {
+				aes.CTR(key, iv, msg, msg)
+			}
+		})
+	}
+}
+
+//nolint:gochecknoglobals // this is fine
+var lengths = []struct {
+	name string
+	n    int
+}{
+	{"16B", 16},
+	{"32B", 32},
+	{"64B", 64},
+	{"128B", 128},
+	{"256B", 256},
+	{"512B", 512},
+	{"1KiB", 1024},
+	{"2KiB", 2048},
+}
